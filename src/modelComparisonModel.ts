5df454d185e8aefb3d57714b2ed86e926cd8c175
Klaus Eckelt <Klaus.Eckelt@jku.at>
2022-02-10 11:54:24 +0100

    Fix Github Action build errors (#2)

--- src/modelComparisonModel.ts
+++ src/modelComparisonModel.ts
@@ -1,78 +1,80 @@
-import {ISessionContext} from "@jupyterlab/apputils";
+import { ISessionContext } from '@jupyterlab/apputils';
 import { IOutput } from '@jupyterlab/nbformat';
 import { Kernel, KernelMessage } from '@jupyterlab/services';
 import { ISignal, Signal } from '@lumino/signaling';
 
 export class ModelComparisonModel {
-    private _future: Kernel.IFuture<KernelMessage.IExecuteRequestMsg, KernelMessage.IExecuteReplyMsg> | null = null;
-    private _stateChanged = new Signal<ModelComparisonModel, void>(this);
-    private _output: IOutput | null = null;
-    private _sessionContext: ISessionContext;
+  private _future: Kernel.IFuture<
+    KernelMessage.IExecuteRequestMsg,
+    KernelMessage.IExecuteReplyMsg
+  > | null = null;
+  private _stateChanged = new Signal<ModelComparisonModel, void>(this);
+  private _output: IOutput | null = null;
+  private _sessionContext: ISessionContext;
 
-    private _isFilterActive: boolean = false;
+  private _isFilterActive = false;
 
+  constructor(_sessionContext: ISessionContext) {
+    this._sessionContext = _sessionContext;
+  }
 
-    constructor(_sessionContext: ISessionContext) {
-        this._sessionContext = _sessionContext;
+  get future(): Kernel.IFuture<
+    KernelMessage.IExecuteRequestMsg,
+    KernelMessage.IExecuteReplyMsg
+  > | null {
+    return this._future;
+  }
+
+  set future(
+    value: Kernel.IFuture<
+      KernelMessage.IExecuteRequestMsg,
+      KernelMessage.IExecuteReplyMsg
+    > | null
+  ) {
+    this._future = value;
+    if (!value) {
+      return;
     }
+    value.onIOPub = this._onIOPub;
+  }
 
-    get future(): Kernel.IFuture<
-        KernelMessage.IExecuteRequestMsg,
-        KernelMessage.IExecuteReplyMsg
-        > | null {
-        return this._future;
+  execute(code: string): void {
+    if (!this._sessionContext || !this._sessionContext.session?.kernel) {
+      return;
+    } else {
+      this.future = this._sessionContext.session.kernel.requestExecute({
+        code
+      });
     }
+    this._isFilterActive = !this._isFilterActive;
+    this._stateChanged.emit();
+  }
 
-    set future(
-        value: Kernel.IFuture<
-            KernelMessage.IExecuteRequestMsg,
-            KernelMessage.IExecuteReplyMsg
-            > | null
-    ) {
-        this._future = value;
-        if (!value) {
-            return;
-        }
-        value.onIOPub = this._onIOPub;
-    }
-
-    execute(code: string): void {
-        if (!this._sessionContext || !this._sessionContext.session?.kernel) {
-            return;
-        } else {
-            this.future = this._sessionContext.session.kernel.requestExecute({
-                code,
-            });
-        }
-        this._isFilterActive = !this._isFilterActive;
+  private _onIOPub = (msg: KernelMessage.IIOPubMessage): void => {
+    const msgType = msg.header.msg_type;
+    switch (msgType) {
+      case 'execute_result':
+      case 'display_data':
+      case 'update_display_data':
+        this._output = msg.content as IOutput;
+        console.log(this._output);
         this._stateChanged.emit();
+        break;
+      default:
+        break;
     }
+    return;
+  };
 
-    private _onIOPub = (msg: KernelMessage.IIOPubMessage): void => {
-        const msgType = msg.header.msg_type;
-        switch (msgType) {
-            case 'execute_result':
-            case 'display_data':
-            case 'update_display_data':
-                this._output = msg.content as IOutput;
-                console.log(this._output);
-                this._stateChanged.emit();
-                break;
-            default:
-                break;
-        }
-        return;
-    };
+  get output(): IOutput | null {
+    return this._output;
+  }
 
-    get output(): IOutput | null {
-        return this._output;
-    }
+  get stateChanged(): ISignal<ModelComparisonModel, void> {
+    return this._stateChanged;
+  }
 
-    get stateChanged(): ISignal<ModelComparisonModel, void> {
-        return this._stateChanged;
-    }
-
-    get isFilterActive(): boolean {
-        return this._isFilterActive;
-    }
+  get isFilterActive(): boolean {
+    return this._isFilterActive;
+  }
 }
